7bd3c26f0645fb8f3eec59c86cac7e00585c6a4d
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-03 23:52:32 +0500

    update

--- apps/card_block/views.py
+++ apps/card_block/views.py
@@ -1,13 +1,13 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from apps.forms import UserForm
-from apps.models import User
+from apps.card_block.forms import UserForm
+from apps.card_block.models import User
 
 
 class HomeListView(ListView):
     queryset = User.objects.all()
-    template_name = 'apps/index.html'
+    template_name = 'apps/card_block/index.html'
     context_object_name = 'users'
 
 
@@ -15,17 +15,17 @@ class AddPersonView(CreateView):
     form_class = UserForm
     queryset = User.objects.all()
     success_url = reverse_lazy('home')
-    template_name = 'apps/add.html'
+    template_name = 'apps/card_block/add.html'
 
 
 class UpdatePersonView(UpdateView):
     queryset = User.objects.all()
     form_class = UserForm
     success_url = reverse_lazy('home')
-    template_name = 'apps/edit.html'
+    template_name = 'apps/card_block/edit.html'
 
 
 class DeletePersonView(DeleteView):
     queryset = User.objects.all()
     success_url = reverse_lazy('home')
-    template_name = 'apps/delete.html'
+    template_name = 'apps/card_block/delete.html'
