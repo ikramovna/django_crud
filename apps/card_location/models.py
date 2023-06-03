b53951cfed66ff3e15486ffd2573fd1cd70885ca
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-03 14:55:47 +0500

    update

--- apps/card_location/models.py
+++ apps/card_location/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    job = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='users/', null=True, blank=True)
--- apps/card_location/views.py
+++ apps/card_location/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from apps.card_location.forms import UserForm
+from apps.card_location.models import User
+
+
+def home(request):
+    user = User.objects.all()
+    context = {
+        'users': user
+    }
+    return render(request, 'apps/card_location/index.html', context)
+
+
+def add_person(request):
+    if request.method == "POST":
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    return render(request, 'apps/card_location/add.html')
+
+
+def update_person(request, pk):
+    user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    context = {'user': user}
+    return render(request, 'apps/card_location/edit.html', context)
+
+
+def delete_person(request, pk):
+    user = User.objects.filter(id=pk)
+    user.delete()
+    return redirect('/')
